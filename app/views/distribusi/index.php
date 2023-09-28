42ce69dac9b321d4d0ab3e17f43a117f7012e861
Alifvidi <muhammadalifvidi@gmail.com>
2023-09-28 14:40:44 +0700

    distribusi

---
+++ app/views/distribusi/index.php
@@ -0,0 +1,15 @@
+<div class="container" style="margin-top: 100px;">
+    <h1 style="text-align: center;">Distribusi</h1>
+    <div class="dropdown" >
+        <a class="btn btn-lg active dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="background-color: unset; outline: none; border: none;">
+            <img src="<?=BASEURL?>/img/filter.png" alt="" style="width: 82px; height: 36px;">
+        </a>
+
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+    </div>
+    <img src="<?=BASEURL?>/img/map-distribusi.png" alt="" style="width: 1298px;">
+</div>
