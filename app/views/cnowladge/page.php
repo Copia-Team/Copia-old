84cfad10e2a04499835ff803daf807933a9071c3
salman4ll <sfarisi0101@gmail.com>
2023-09-28 13:48:54 +0700

    cnowladge

---
+++ app/views/cnowladge/page.php
@@ -0,0 +1,31 @@
+
+<img src="<?=BASEURL?>/img/page.png" class="img-fluid" alt="..." style="margin-bottom: 20px;">
+
+<div class="container mt-5>
+<p class="card-text"><small class="text-body-secondary">Minggu, 25 Jul 2021 09:42 WIB</small></p>
+<p>
+    <pre>
+Jakarta, CNN Indonesia -- Selain nasi, jagung manis juga merupakan sumber karbohidrat yang baik bagi tubuh. Jagung manis juga diperkaya dengan vitamin, dan mineral. 
+Studi menunjukkan sekitar 164 gram jagung manis mengandung 177 kalori, 41 gram karbohidrat, 5,4 gram protein, 2,1 gram lemak, 4,6 serat.
+
+Varian jagung manis ini juga mengandung vitamin C (17 persen dari kebutuhan harian), vitamin B1 (24 persen dari kebutuhan harian), vitamin B9 (19 persen dari kebutuhan 
+harian), magnesium (11 persen dari kebutuhan harian) dan potasium (10 persen dari kebutuhan harian).
+
+Anda dapat mengolah jagung manis dengan sejumlah cara mudah dan sederhana. Tanpa menambahkan banyak bumbu, rasa manis alami pada jagung manis dapat membuat lidah 
+bergoyang. 
+
+Berikut 5 cara mudah olah jagung manis:
+
+<b>1. Kukus</b>
+Teknik rebus membuat jagung manis mengandung lebih banyak air. Anda bisa mencoba teknik kukus sehingga jagung bakal mengandung sedikit air dan mempertahankan tekstur 
+yang renyah.Cukup siapkan panci kukusan yang memiliki wadah berlubang-lubang kecil. Letakkan jagung yang sudah dikupas pada wadah, lalu tutup setelah air mendidih. 
+Proses ini hanya memerlukan waktu 3-5 menit. Setelah matang, jagung bisa langsung dikonsumsi atau diberi bumbu berupa mentega, garam, merica atau bubuk cabai.Melansir 
+Taste of Home, mengukus jagung merupakan cara yang sehat karena proses ini menjaga nutrisi jagung lebih baik daripada merebus.
+    
+<b> Bakar (grill)</b>
+Jagung manis juga bisa dibakar. Proses grilling bisa dilakukan dengan menyertakan kulit jagung atau dikupas bersih terlebih dahulu. Melansir The Spruce Eats, jika 
+dikupas jagung sebaiknya diberi lapisan mentega cair untuk memberikan rasa gurih.Bakar jagung dalam api sedang dan bolak-balik selama kurang lebih 15 menit hingga 
+matang merata. Tak perlu khawatir dengan bagian jagung yang agak gosong karena akan memberikan sensasi 'smoky'.
+    </pre>
+</p>
+</div>
